bca4e88049e1d67a995cd52318a615b64ed799ea
Tiến Lực Nguyễn <matkinhig@gmail.com>
2020-02-26 01:09:00 +0700

    dont create user by channel with goroutine

--- api/controllers/user.go
+++ api/controllers/user.go
@@ -1,13 +1,43 @@
 package controller
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/matkinhig/go-blogs/api/database"
+	"github.com/matkinhig/go-blogs/api/models"
+	"github.com/matkinhig/go-blogs/api/repository"
+	"github.com/matkinhig/go-blogs/api/repository/crud"
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("List users"))
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create user"))
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	u := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repo := crud.NewRepositoryUserCRUD(db)
+	func(u repository.UserRepository) {
+		newUser, err := u.Save(u)
+	}(repo)
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
